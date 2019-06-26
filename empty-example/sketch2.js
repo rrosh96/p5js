2acb9c5dc39f836b40e609c0957f333b011ff110
Roshan Raju <roshanraju@Roshans-MacBook-Pro.local>
2019-06-26 11:34:22 +0530

    Updated sketch 2 and index.html routes

--- empty-example/sketch2.js
+++ empty-example/sketch2.js
@@ -6,7 +6,7 @@ function draw () {
     background(127);
     noStroke();
     for (let i =0; i < height ; i += 20){
-        fill (129, 206, 15);
+        fill (220,20,60);
         rect (0 , i, width, 10) 
         fill (255);
         rect (i, 0, 10, height)
